b3e68b500dd750f51ff8cd56d4d1b4450e98ed6b
Marcos Andre <marcosdev.me@gmail.com>
2022-05-15 10:13:55 -0300

    feat: adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import SplashScreen from "./pages/SplashScreen";
 
-const App = () => <SplashScreen />;
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<SplashScreen />} />
+
+      <Route path="*" element={<h1>Not Found...</h1>} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
